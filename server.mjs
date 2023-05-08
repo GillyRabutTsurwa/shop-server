73dbe4aaa64ee480c4a37a1fb46e8cdf6ace51cd
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-05-07 20:09:00 -0400

    Fixed Server Issue - i did this by correcting the success and cancel url properties - i also made a html page that express will serve - this is so that my api page will have something in the index page - i will worry about styling it later

--- server.mjs
+++ server.mjs
@@ -21,6 +21,7 @@ const clientDevUrl = process.env.CLIENT_DEV_URL;
 const CLIENT_URL = isProduction ? clientProdUrl : clientDevUrl;
 
 app.use(express.json());
+app.use(express.static("public"));
 app.use(
     cors({
         origin: CLIENT_URL,
@@ -31,12 +32,9 @@ dotenv.config();
 const stripeProdKey = process.env.STRIPE_PRIVATE_PROD_KEY;
 const stripeDevKey = process.env.STRIPE_PRIVATE_TEST_KEY;
 
-const STRIPE_KEY = isProduction ? stripeProdKey : stripeDevKey;
-
-const stripe = new Stripe(STRIPE_KEY);
-
+const STRIPE_SECRET_KEY = isProduction ? stripeProdKey : stripeDevKey;
+const stripe = new Stripe(STRIPE_SECRET_KEY);
 const products = await fetchProducts();
-// console.log(products);
 
 app.get("/products", (req, res) => {
     res.send(products);
@@ -46,26 +44,13 @@ app.post("/checkout", async (request, response) => {
     // removing this stripe code session below and making it its own variable
     const lineItems = request.body.items.map((currentItem) => {
         const storeItem = products.find((currentProduct) => currentProduct._id === currentItem.id);
-        // console.log(urlFor(storeItem.mainImage.asset._ref).options.source);
-        // TESTING: working on showing images show in checkout
         const str = storeItem.mainImage.asset._ref;
-        console.log(str.slice(6, str.length - 4));
-        console.log(urlFor(storeItem.mainImage));
-        console.log(urlFor(str));
         const { baseUrl, projectId, dataset, source } = urlFor(str).options;
-        console.log(`baseURL: ${baseUrl}`);
-        console.log(`projectId: ${projectId}`);
-        console.log(`dataset: ${dataset}`);
-        console.log(`source: ${source}`);
-        console.log(`${baseUrl}/images/${projectId}/${dataset}/${str.slice(6, str.length - 4)}`);
         return {
             price_data: {
                 currency: "usd",
                 product_data: {
                     name: storeItem.name,
-                    // images: [
-                    //     "https://plus.unsplash.com/premium_photo-1674147605306-7192b6208609?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-                    // ],
                     images: [`${baseUrl}/images/${projectId}/${dataset}/${str.slice(6, str.length - 4)}.jpg`],
                 },
                 unit_amount: storeItem.price,
@@ -81,9 +66,8 @@ app.post("/checkout", async (request, response) => {
             payment_method_types: ["card"],
             mode: "payment",
             line_items: lineItems,
-            // success_url: `${process.env.CLIENT_URL}/about`,
-            success_url: `${process.env.CLIENT_URL}`,
-            cancel_url: `${process.env.CLIENT_URL}`,
+            success_url: `${CLIENT_URL}/success`,
+            cancel_url: `${CLIENT_URL}`,
         });
         response.json({ url: session.url });
         // return response.send({ url: session.url });
@@ -98,4 +82,5 @@ app.post("/checkout", async (request, response) => {
 //NOTE: make our application work
 app.listen(PORT, () => {
     console.log(`Server Running on Port ${PORT}`);
+    console.log(`Current Client is ${CLIENT_URL}`);
 });
